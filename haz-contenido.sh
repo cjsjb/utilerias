b2bc723e811b5f2f9e308773bb68b3d27145c171
Daniel Díaz <yosoy@danieldiaz.org>
2018-08-23 18:51:14 -0500

    haz-contenido: Cambiar base, def. de libro, modo silencioso.
    Signed-off-by: Daniel Díaz <yosoy@danieldiaz.org>

--- haz-contenido.sh
+++ haz-contenido.sh
@@ -9,16 +9,13 @@
 WORKOUTPUT="contenido.pdf"
 WORKDIR=$(pwd)
 INDEX=${WORKDIR}/indice.txt
-BASEDIR=$(dirname $0)
+UTILDIR=$(readlink -f $(dirname $0))
 
 THREAD=""
 declare -a pagealignment
 pagealignment[0]="l"
 pagealignment[1]="r"
 
-
-#rm -rf partituras
-mkdir -p partituras
 
 [ -e ${INDEX} ] && rm ${INDEX}
 CUENTA=1
@@ -32,6 +29,8 @@ while read -r i; do
 		arch="${i:2}"
 		pagealignwant="${i:0:1}"
 	fi
+	descr="$(echo $arch | cut -d: -f2)"
+	arch="$(echo $arch | cut -d: -f1)"
 
 	eldir=$(dirname $arch)
 	localdir=${eldir#*\/}
@@ -44,38 +43,41 @@ while read -r i; do
 		currentpagemod=$( echo "${CUENTA} % 2" | bc)
 		currentpagealignment=${pagealignment[${currentpagemod}]}
 		[ -n "${pagealignwant}" ] && if [ ! "${currentpagealignment}" = "${pagealignwant}" ]; then
-			THREAD="${THREAD} ${BASEDIR}/extras/hoja-blanca.pdf"
+			THREAD="${THREAD} ${UTILDIR}/paginas/hoja-blanca.pdf"
 			CUENTA=$(( $CUENTA + 1 ))
 		fi
 
-		perl -i -p -e "s#first-page-number = [0-9]*\$#first-page-number = ${CUENTA}#g" ${localfile}
+		sed -i -e "s#first-page-number = [0-9]*\$#first-page-number = ${CUENTA}#g" ${localfile}
 
 		TITLE=$(grep title ${localfile} | grep -v subtitle | egrep -o \".*\"$ | tr -d \")
 		SUBTITLE=$(grep subtitle ${localfile} | grep -v subsubtitle | egrep -o \".*\"$ | tr -d \")
-		IENTRY="${TITLE}"; [ -n "${SUBTITLE}" ] && IENTRY="${IENTRY} ${SUBTITLE}"
+		IENTRY="${TITLE}"
+		[ -n "${SUBTITLE}" ] && IENTRY="${IENTRY} ${SUBTITLE}"
 		echo ${CUENTA},${IENTRY} >> ${INDEX}
 
-		[ -x ${WORKDIR}/postprocessor.sh ] && bash ${WORKDIR}/postprocessor.sh ${localfile}
+		[ -x ${WORKDIR}/postprocessor.sh ] && bash ${WORKDIR}/postprocessor.sh $(readlink -f ${localfile})
 
-		lilypond ${localfile}
+		echo -n "${localfile}... "
+		lilypond -s ${localfile}
 		rm -f ${localfile%%.ly}.ps
 		pdfpages=$(strings ${localfile%%.ly}.pdf | egrep -o '/Count\ [0-9]*$' | sed -e 's#\/Count\ ##g')
-		echo "Pages=[${pdfpages}]"
+		echo "${pdfpages} page(s)"
 		THREAD="${THREAD} partituras/${localdir}/${localfile%%.ly}.pdf"
 
 		popd >/dev/null
-	elif [ "${filetype}" = "pdf" -a -e "${BASEDIR}/${arch}" ]; then
+	elif [ "${filetype}" = "pdf" -a -e "${arch}" ]; then
+		echo -n "${descr}... "
 		currentpagemod=$( echo "${CUENTA} % 2" | bc)
 		currentpagealignment=${pagealignment[${currentpagemod}]}
 		[ -n "${pagealignwant}" ] && if [ ! "${currentpagealignment}" = "${pagealignwant}" ]; then
-			THREAD="${THREAD} ${BASEDIR}/extras/hoja-blanca.pdf"
+			THREAD="${THREAD} ${UTILDIR}/paginas/hoja-blanca.pdf"
 			CUENTA=$(( $CUENTA + 1 ))
 		fi
 
-		echo ${CUENTA},${localfile} >> ${INDEX}
-		pdfpages=$(strings ${BASEDIR}/${localdir}/${localfile} | egrep -o '/Count\ [0-9]*$' | sed -e 's#\/Count\ ##g')
-		echo "Pages=[${pdfpages}]"
-		THREAD="${THREAD} ${BASEDIR}/${localdir}/${localfile}"
+		echo ${CUENTA},${localfile},${descr} >> ${INDEX}
+		pdfpages=$(strings ${UTILDIR}/${localdir}/${localfile} | egrep -o '/Count\ [0-9]*$' | sed -e 's#\/Count\ ##g')
+		echo "${pdfpages} page(s)"
+		THREAD="${THREAD} ${UTILDIR}/${localdir}/${localfile}"
 	fi
 
 	[ -z "${pdfpages}" ] && pdfpages=0
@@ -83,4 +85,4 @@ while read -r i; do
 
 done < libro.def
 
-yes quit | gs -sDEVICE=pdfwrite -sOutputFile=${WORKOUTPUT} ${THREAD}
+gs -q -sDEVICE=pdfwrite -dBATCH -dNOPAUSE -sOutputFile=${WORKOUTPUT} ${THREAD}
